e302332b77867fb0774a2b89a1ff4dc869760dd0
ashutosh <ashutosh@tothenew.com>
2024-12-30 18:09:09 +0530

    main: code setup

---
+++ src/main/java/com/ttn/punchingSystem/PunchingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ttn.punchingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttn/punchingSystem/PunchingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.punchingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

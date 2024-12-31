ece94effe420007e6bec12e1ed83a1b9b550ead8
ashutosh <ashutosh@tothenew.com>
2024-12-31 14:08:36 +0530

    main: code in progress for punchingSystem

--- src/main/java/com/ttn/punchingSystem/controller/CsvController.java
+++ src/main/java/com/ttn/punchingSystem/controller/CsvController.java
@@ -4,20 +4,23 @@ import com.ttn.punchingSystem.model.PunchData;
 import com.ttn.punchingSystem.service.CsvReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class CsvController {
 
     @Autowired
     private CsvReaderService csvReaderService;
 
     @GetMapping("/read-csv")
-    public List<PunchData> readCsv() {
-        String fileName = "";
-        return csvReaderService.readCsvFile(fileName);
+    public List<PunchData> readCsv(@RequestParam("filePath") String filePath) throws ParseException {
+        return csvReaderService.readCsvFile(filePath);
     }
 
 }

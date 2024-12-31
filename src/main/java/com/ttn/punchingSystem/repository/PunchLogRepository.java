c24c4f111d4a308dd2bb2505804b429689f406d8
ashutosh <ashutosh@tothenew.com>
2024-12-31 15:21:18 +0530

    main: code in progress for punchingSystem

--- src/main/java/com/ttn/punchingSystem/repository/PunchLogRepository.java
+++ src/main/java/com/ttn/punchingSystem/repository/PunchLogRepository.java
@@ -1,12 +1,12 @@
 package com.ttn.punchingSystem.repository;
-import com.ttn.punchingSystem.model.PunchLog;
+import com.ttn.punchingSystem.model.PunchingDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
 import java.util.List;
 
-public interface PunchLogRepository extends JpaRepository<PunchLog, Long> {
+public interface PunchLogRepository extends JpaRepository<PunchingDetails, Long> {
 
-    List<PunchLog> findByUserEmailAndPunchDate(String userEmail, Date punchDate);
+    List<PunchingDetails> findByUserEmailAndPunchDate(String userEmail, Date punchDate);
 }
 

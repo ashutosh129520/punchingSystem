cf76f07f4c2f84d4fba0125d67d44e9f609ba222
ashutosh <ashutosh@tothenew.com>
2025-01-01 19:21:27 +0530

    main: Code done to save csv data into DB and check validations. Made a cleaner code and used mapper to set values

---
+++ src/main/java/com/ttn/punchingSystem/model/WorkScheduleDetails.java
@@ -0,0 +1,33 @@
+package com.ttn.punchingSystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkScheduleDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "project_id")
+    private Long projectId;
+
+    @Column(name = "work_shift", nullable = false)
+    private String workShift;
+
+    @Column(name = "office_days")
+    private String officeDays;
+
+    // Getters and Setters
+}
+

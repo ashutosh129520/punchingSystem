85aa6f6f49f019d1a5117e727ae60348f0b820c0
ashutosh <ashutosh@tothenew.com>
2025-01-02 11:29:45 +0530

    main: Code in progress

--- src/main/java/com/ttn/punchingSystem/controller/CsvController.java
+++ src/main/java/com/ttn/punchingSystem/controller/CsvController.java
@@ -2,6 +2,7 @@ package com.ttn.punchingSystem.controller;
 
 import com.ttn.punchingSystem.model.PunchingDetailsDTO;
 import com.ttn.punchingSystem.service.CsvReaderService;
+import com.ttn.punchingSystem.utils.InvalidPunchTimeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,7 @@ public class CsvController {
     private CsvReaderService csvReaderService;
 
     @GetMapping("/read-csv")
-    public ResponseEntity<List<PunchingDetailsDTO>> readCsv(@RequestParam("filePath") String filePath) throws ParseException {
+    public ResponseEntity<List<PunchingDetailsDTO>> readCsv(@RequestParam("filePath") String filePath) throws ParseException, InvalidPunchTimeException {
         return csvReaderService.readCsvFile(filePath);
     }
 

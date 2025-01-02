14cf9753f9be46ea4d9cec70eb875e4e32381b9c
ashutosh <ashutosh@tothenew.com>
2025-01-02 13:44:02 +0530

    main: Code in progress code seperation of concerns

--- src/main/java/com/ttn/punchingSystem/utils/AppConstant.java
+++ src/main/java/com/ttn/punchingSystem/utils/AppConstant.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class AppConstant {
     public static final String DATE_FORMAT = "dd MMM yyyy h:mm a";
     public static final Pattern EMAIL_PATTERN = Pattern.compile(
-            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+            "^[A-Za-z0-9+_-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
     );
     public static final Pattern FILE_NAME_PATTERN = Pattern.compile(
             "^\\d{2}[A-Za-z]{3}\\d{4}_punchdetails\\.csv$"

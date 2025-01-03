504853d061e966f2c63ce22eaaa27718fa50c622
ashutosh <ashutosh@tothenew.com>
2025-01-03 11:30:52 +0530

    main: Code completion of csv read and save

--- src/main/java/com/ttn/punchingSystem/utils/CsvValidationException.java
+++ src/main/java/com/ttn/punchingSystem/utils/CsvValidationException.java
@@ -3,15 +3,9 @@ package com.ttn.punchingSystem.utils;
 import java.util.List;
 
 public class CsvValidationException extends RuntimeException {
-    private List<String> errorMessages;
 
     public CsvValidationException(List<String> errorMessages) {
-        super("Validation errors occurred while processing the CSV file.");
-        this.errorMessages = errorMessages;
-    }
-
-    public List<String> getErrorMessages() {
-        return errorMessages;
+        super("Validation errors occurred while processing the CSV file: "+errorMessages);
     }
 }
 
